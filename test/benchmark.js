efc567c40c27434b47d565393c313287e92810b1
475218106@qq.com <475218106@qq.com>
2018-11-01 14:56:06 +0800

    ‘添加benchmark’

---
+++ test/benchmark.js
@@ -0,0 +1,39 @@
+const {num1, num2} = require('../src/fn')
+const Benchmark = require('benchmark')
+
+var suite = new Benchmark.Suite;
+
+suite.add('parseInt', () => {
+  num1('1111')
+})
+.add('Nmber', () => {
+  num2('1111')
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({ 'async': true });
+
+// add tests
+// suite.add('RegExp#test', function() {
+//   /o/.test('Hello World!');
+// })
+// .add('String#indexOf', function() {
+//   'Hello World!'.indexOf('o') > -1;
+// })
+// .add('String#match', function() {
+//   !!'Hello World!'.match(/o/);
+// })
+// // add listeners
+// .on('cycle', function(event) {
+//   console.log(String(event.target));
+// })
+// .on('complete', function() {
+//   console.log('Fastest is ' + this.filter('fastest').map('name'));
+// })
+// // run async
+// .run({ 'async': true });
